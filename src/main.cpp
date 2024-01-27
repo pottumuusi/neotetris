e68b60caec7463d2d9e662c03fb968bb97d2c36d
Aleksi Backman <arbiter0xf@protonmail.com>
2024-01-27 12:19:11 +0200

    feat: add build in addition to sdl bringup and teardown

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,48 @@
 #include "Log.hpp"
 
+#ifdef __linux__
+#include <SDL2/SDL.h>
+#endif
+
+#ifdef _WIN32
+#include <SDL.h>
+#endif
+
 const char* g_program_name = "neotetris";
 
 static void game(void)
 {
+    std::int32_t ret;
+    std::uint32_t flags;
+
     std::string msg;
+
+    ret = -1;
+    flags = 0;
 
     msg = g_program_name;
     msg += " running";
     Log::i(msg);
 
+    // SDL init
+    flags = SDL_INIT_VIDEO;
+    ret = SDL_Init(flags);
+
+    // SDL setHint
+
+    // SDL imgInit # <- necessary here?
+
+    // SDL createMainWindow
+
+    // SDL createMainRenderer # <- necessary here?
+
+    // SDL setRenderDrawColor # <- necessary here?
+
     msg = g_program_name;
     msg += " shutting down";
     Log::i(msg);
+
+    SDL_Quit();
 }
 
 // Disable name mangling so that SDL can find and redefine main.
