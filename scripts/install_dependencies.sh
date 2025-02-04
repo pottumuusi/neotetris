f6a9913b194b08610d8f877d627548f916107ddd
Aleksi Backman <bddubbb@protonmail.com>
2025-02-04 18:22:31 +0200

    Add optional SDL install to script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+readonly INSTALL_SDL="FALSE"
+
 main() {
     local -r workarea='neotetris_dependencies_install'
 
     pushd /tmp
     mkdir "${workarea}"
     pushd "${workarea}"
+
+    if [ "TRUE" == "$INSTALL_SDL" ] ; then
+        sudo apt install -y libx11-dev libxext-dev
+
+        wget https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.24.0.zip
+        unzip release-2.24.0.zip
+        pushd SDL-release-2.24.0
+        ./configure
+        make
+        sudo make install
+        popd
+    fi
 
     git clone https://github.com/pottumuusi/vulkan-tools.git
 
